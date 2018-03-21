cb96060796ab3bd6f326349bcc2897f93e9af6c0
Tarique Sani <tarique@sanisoft.com>
2018-03-21 16:28:05 +0530

    Fixed seconds part of Pace not showing correctly

--- content.js
+++ content.js
@@ -19,11 +19,10 @@ function main(userPrefs){
 
     $.ajax(settings).done(function (response) {
       timeElapsed = $( "[data-glossary-term='definition-elapsed-time']" ).parent().next().html();
-      //console.log($( "[data-glossary-term='definition-elapsed-time']" ).parent().next().html());
 
       if (response.type == "Run") {
-        totalPace = ((response.elapsed_time/60)/(response.distance/1000));
-        totalPace = totalPace.toFixed(2);
+        totalPace = ((response.elapsed_time)/(response.distance/1000));
+        totalPace = fmtMSS(totalPace.toFixed(0));
         timeElapsed += " ("+totalPace+"/km)";        
       } else {
         totalSpeed = ((response.distance/response.elapsed_time)/1000)*3600;
@@ -34,5 +33,8 @@ function main(userPrefs){
     	$("[data-glossary-term='definition-elapsed-time']").parent().next().html(timeElapsed);
     });
 }
+
+// From https://stackoverflow.com/questions/3733227/javascript-seconds-to-minutes-and-seconds 
+function fmtMSS(s){return(s-(s%=60))/60+(9<s?':':':0')+s}
 
 chrome.storage.sync.get('authKey', main);
